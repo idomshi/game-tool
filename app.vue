b5a38989a3ceece8bce19ca5e89df71ccb1005c2
idomshi <56523771+idomshi@users.noreply.github.com>
2022-12-04 07:21:09 +0000

    メイン画面のモックアップレイアウトを始めました

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <h1>game-tool</h1>
+  <div class="w-full h-screen flex flex-col">
+    <header class="h-12 px-2 shrink-0 bg-slate-50 flex items-center justify-left">
+      <h1>game-tool</h1>
+    </header>
+    <div class="h-full p-2">
+      <ul class="flex flex-col gap-2">
+        <TextCard text="「・・・・」"></TextCard>
+        <TextCard text="「ぉーい」"></TextCard>
+      </ul>
+    </div>
   </div>
 </template>
---
+++ components/TextCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+    text: string;
+}>()
+</script>
+
+<template>
+    <li class="border rounded-lg">
+        <h2>{{ text }}</h2>
+    </li>
+</template>
